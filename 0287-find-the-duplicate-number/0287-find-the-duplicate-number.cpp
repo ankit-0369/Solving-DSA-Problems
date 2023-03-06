79ecaa638122a20173751243aa092e89ef08c9da
ankit-0369 <98079557+ankit-0369@users.noreply.github.com>
2023-03-06 14:46:39 +0530

    Time: 121 ms (65.92%), Space: 61.3 MB (67.23%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,19 +3,25 @@ public:
     int findDuplicate(vector<int>& nums) {
             
         //negative marking approach---
-        int i=0;
-        int res=-1;
-        while(i<nums.size()){
+//         int i=0;
+//         int res=-1;
+//         while(i<nums.size()){
         
-            nums[abs(nums[i])]*= -1;
-            if(nums[abs(nums[i])]>0){
+//             nums[abs(nums[i])]*= -1;
+//             if(nums[abs(nums[i])]>0){
 
-                return abs(nums[i]);
+//                 return abs(nums[i]);
                
-            }
-            i++;
+//             }
+//             i++;
+//         }
+        //without modifiying array---
+        
+        for(int i=0; i<nums.size(); i++){
+            
+            swap(nums[0], nums[nums[0]]);
         }
       
-          return res;
+          return nums[0];
     }
 };
