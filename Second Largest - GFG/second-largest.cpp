baef4cd5d7526800ef1ac452590f09fb45ca47d1
ankit-0369 <98079557+ankit-0369@users.noreply.github.com>
2023-03-20 15:48:34 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -15,15 +15,15 @@ public:
 	    int l2= -1;
 	    for(int i=0; i<n; i++){
 	       if(arr[i]>l1){
+	           l2= l1;
 	           l1= arr[i];
 	       }
-	          
-	    }
-	    for(int i=0; i<n; i++){
 	        if(arr[i]>l2 && arr[i]<l1){
 	            l2= arr[i];
 	        }
+	          
 	    }
+	    
 	    
 	    return l2;
 	}
