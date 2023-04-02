80fbbb5ae486093e467a7c43c12f243fe74d70e3
ankit-0369 <98079557+ankit-0369@users.noreply.github.com>
2023-04-02 21:45:07 +0530

    Time: 5 ms (31.73%), Space: 5.9 MB (92.46%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) { //t.c- O(log5n) & auxilary-space:- O(1)
+        int cntZero=0;
+        while(n!=0){
+            cntZero+= (n/5);
+            n/= 5;
+        }
+        
+        return cntZero;
+    }
+};
