759117980d02462072aec153b75de61c3242cbc8
ankit-0369 <98079557+ankit-0369@users.noreply.github.com>
2023-04-10 16:47:50 +0530

    Time: 4 ms (39.51%), Space: 6.3 MB (56.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,17 +1,35 @@
 class Solution {
+    private:
+//     bool solve1(string s){
+//                stack<char> st;
+//        for(auto c:s){
+//            if(c=='(' || c=='{' || c=='['){
+//                st.push(c);
+//            }else{
+//                if(st.empty() || (c==')' && st.top()!='(')||(c=='}' && st.top()!='{') || (c==']' && st.top()!='[') )
+//                return false;
+//                st.pop();
+//            }
+//        }
+
+//        return st.empty();
+//     }
+    bool solve2(string s){
+        vector<char> st;
+        for(auto c: s){
+            if(c=='(' or c=='{' or c=='['){
+                st.push_back(c);
+            }else{
+                if(st.empty() or (st[st.size()-1]=='(' and c!=')') or (st[st.size()-1]=='{' and c!='}') or (st[st.size()-1]=='[' and c!=']'))
+                return false;
+                st.pop_back();
+            }
+        }
+        return st.empty();
+    }
 public:
     bool isValid(string s) {//t.c:- O(n) &  auxilary-space:- O(n)--------
-       stack<char> st;
-       for(auto c:s){
-           if(c=='(' || c=='{' || c=='['){
-               st.push(c);
-           }else{
-               if(st.empty() || (c==')' && st.top()!='(')||(c=='}' && st.top()!='{') || (c==']' && st.top()!='[') )
-               return false;
-               st.pop();
-           }
-       }
-
-       return st.empty();
+            // return solve1(s);
+        return solve2(s);
     }
 };
