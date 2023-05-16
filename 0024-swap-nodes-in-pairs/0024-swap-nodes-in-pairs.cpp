5de49691371024a687dffc05eac490fc8e24437a
ankit-0369 <98079557+ankit-0369@users.noreply.github.com>
2023-05-16 22:27:13 +0530

    Time: 3 ms (58.57%), Space: 7.6 MB (50.79%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -12,11 +12,32 @@ class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         //t.c:- O(n) & auxilary-space:- O(1)
-        ListNode* temp= head;
-        while(temp!= NULL && temp->next!= NULL){
-            swap(temp->val, temp->next->val);
-            temp= temp->next->next;
+        //naive approach-----
+        // ListNode* temp= head;
+        // while(temp!= NULL && temp->next!= NULL){
+        //     swap(temp->val, temp->next->val);
+        //     temp= temp->next->next;
+        // }
+        // return head;
+        //swapping complete Nodes(Not only values)-----
+
+        if(head==NULL || head->next==NULL){
+            return head;
         }
-        return head;
+        ListNode* temp= new ListNode();
+        temp->next= head;
+        ListNode* prev= temp;
+        ListNode* cur= head;
+        while(cur && cur->next){
+            prev->next= cur->next;
+            cur->next= cur->next->next;
+            prev->next->next= cur;
+            prev= cur;
+            cur= cur->next;
+
+        }
+
+        return temp->next;
+
     }
 };
