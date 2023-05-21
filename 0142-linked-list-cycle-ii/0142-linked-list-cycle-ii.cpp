44a672df00ef0cac25f534e12e60194c000080ba
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-05-21 22:38:41 +0530

    Time: 8 ms (77.62%), Space: 7.7 MB (60.07%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -7,19 +7,61 @@
  * };
  */
 class Solution {
+    private:
+    // ListNode* naiveApproach(ListNode* head){
+            
+    //     map<ListNode*, bool> m_p;
+    //     ListNode* temp= head;
+    //     while(temp!= NULL){
+    //         if(!m_p.count(temp)){
+    //                 // m_p.insert({temp,true});
+    //                 m_p[temp]= true;
+    //         }else{
+    //             return temp;
+    //         }
+    //         temp= temp->next;
+    //     }
+    //     return NULL;
+    // }
+    
+    ListNode* floydAlgo(ListNode* head){
+            ListNode* slow= head;
+            ListNode* fast= head;
+            while(fast!= NULL && fast->next!= NULL){
+                fast= fast->next->next;
+                slow= slow->next;
+
+                if(fast==slow)
+                return slow;
+            }
+            return NULL;
+    }
+
+    ListNode* optimized(ListNode* head){
+  
+            if(head==NULL)
+            return NULL;
+
+            ListNode* floydPoint= floydAlgo(head);
+            if(!floydPoint)
+            return NULL;
+
+            ListNode* slow= head;
+            while(slow!= floydPoint){
+                floydPoint= floydPoint->next;
+                slow= slow->next;
+            }
+            return slow;
+    }
+    
 public:
     ListNode *detectCycle(ListNode *head) {//t.c:- O(n) & auxilary-space:- O(n)
         //naive approach----using map---
-        map<ListNode*, bool> m_p;
-        ListNode* temp= head;
-        while(temp!= NULL){
-            if(!m_p.count(temp)){
-                    m_p.insert({temp,true});
-            }else{
-                return temp;
-            }
-            temp= temp->next;
-        }
-        return NULL;
+            // t.c:- O(nLogn) & auxilary-space:- O(n)
+        // return naiveApproach(head);
+
+        //using Floyd Algo of Loop detection---
+        // t.c:- O(n) & auxilary-space:- O(1)
+        return optimized(head);
     }
 };
