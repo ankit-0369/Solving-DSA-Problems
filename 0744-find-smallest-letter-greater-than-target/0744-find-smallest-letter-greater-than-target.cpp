82932a6b98f541ffd4ed1b7c5c48c1322111d446
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-06-09 12:09:35 +0530

    Time: 18 ms (47.81%), Space: 15.9 MB (72.43%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -20,14 +20,41 @@ class Solution {
 
         return;
     }
-public:
-    char nextGreatestLetter(vector<char>& letters, char target){
-        //using recursion--- time-complexity:- O(logn) & auxilary-space:- O(1)
-       char res='{'; 
-      cout<<res+1<<endl;
+
+    char recursiveWay(vector<char>& letters, char target){
+        char res='{'; 
+      
       solve(letters,target,0,letters.size()-1, res);
       if(res=='{')
       return letters[0];
       return res;
+    }
+
+    char iterativeWay(vector<char>& letters, char tar){
+        char res= '{';
+        int s=0, e= letters.size()-1;
+        int m= s+(e-s)/2;
+
+        while(s<=e){
+             if(letters[m]>tar){
+                res= min(res, letters[m]);
+                e= m-1;
+            }else{
+                s= m+1;
+            }
+
+            m= s+ (e-s)/2;
+        }
+            if(res=='{')
+            return letters[0];
+        return res;
+    }
+
+public:
+    char nextGreatestLetter(vector<char>& letters, char target){
+        //using recursion--- time-complexity:- O(logn) & auxilary-space:- O(logn)
+    //    return recursiveWay(letters, target);
+
+       return iterativeWay(letters, target);
     }
 };
