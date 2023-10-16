0c92a974334701d26bd3dccbc1b845275806db72
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-10-17 00:32:54 +0530

    Time: 3 ms (36.93%), Space: 6.8 MB (25.89%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     vector<int> getRow(int row) {
-        vector<vector<int>> ans(row+1, vector<int>(1,1));
+//         vector<vector<int>> ans(row+1, vector<int>(1,1));
+//         for(int i=1; i<row+1; i++){
+            
+//             for(int j=1; j<i; j++){
+//                 int ele= ans[i-1][j-1]+ ans[i-1][j];
+//                 ans[i].push_back(ele);
+//             }
+//             ans[i].push_back(1);
+//         }
+        
+//         return ans[row];
+        //optimized soln---
+        vector<int> ans(row+1, 0);
+        ans[0]= 1;
         for(int i=1; i<row+1; i++){
             
-            for(int j=1; j<i; j++){
-                int ele= ans[i-1][j-1]+ ans[i-1][j];
-                ans[i].push_back(ele);
+            for(int j=i; j>=1; j--){
+                
+                ans[j]= ans[j]+ ans[j-1];
             }
-            ans[i].push_back(1);
         }
         
-        return ans[row];
+        return ans;
     }
 };
