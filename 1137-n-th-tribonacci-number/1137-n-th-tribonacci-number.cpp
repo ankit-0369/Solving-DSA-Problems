894dab2b328bfa82c3bed04844f4fddf69344a20
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-10-19 00:22:52 +0530

    Time: 2 ms (49.17%), Space: 6.2 MB (47.11%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -20,7 +20,22 @@ public:
     }
     int tribonacci(int n) {
         
-        vector<int> memo(n+1, -1);
-        return solve(n, memo);
+        // vector<int> memo(n+1, -1);
+        // return solve(n, memo);
+        if(n==0)
+            return 0;
+            
+        
+        int prev3= 0, prev2=1, prev1= 1;
+        for(int i=3; i<=n; i++){
+            
+            int curVal= prev1+ prev2+ prev3;
+            prev3= prev2;
+            prev2= prev1;
+            prev1= curVal;
+            
+        }
+        
+        return prev1;
     }
 };
