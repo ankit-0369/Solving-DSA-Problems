9037e5a3fd4d8ff379e27afd75830003dd591663
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-10-19 00:34:40 +0530

    Time: 3 ms (84.47%), Space: 13.9 MB (65.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -13,14 +13,39 @@ public:
 //     }
     int minCostClimbingStairs(vector<int>& cost) {
         //let dp[i] is the min cost to go from ith step to nth step---
-        int n= cost.size();
-            vector<int> dp(n+2, 0);
-        dp[n]=0;
-        for(int i=n-1; i>=0; i--){
+//         int n= cost.size();
+//             vector<int> dp(n+2, 0);
+//         dp[n]=0;
+//         for(int i=n-1; i>=0; i--){
 
-            dp[i]= cost[i]+ min(dp[i+1] , dp[i+2]);
+//             dp[i]= cost[i]+ min(dp[i+1] , dp[i+2]);
+//         }
+        
+//         return min(dp[0], dp[1]);
+        
+//             int n= cost.size();
+//             vector<int> dp(n+1, -1); //0th to ith step
+//         dp[0]= 0;
+//         for(int i=1; i<=n; i++){
+//             int fs= dp[i-1] + cost[i-1];
+//             int ss= INT_MAX;
+//             if(i>1)
+//                 ss= dp[i-2]+ cost[i-1];
+            
+//             dp[i]= min(fs, ss);
+//         }
+        
+//         return dp[n];
+        int n= cost.size();
+        int next1=0, next2=0;
+        for(int i= n-1; i>=0; i--){
+           int fs= cost[i]+ next1;
+            int ss= cost[i]+ next2;
+            int cur= min(fs, ss);
+            next2= next1;
+            next1= cur;
         }
         
-        return min(dp[0], dp[1]);
+        return min(next1, next2);
     }
 };
