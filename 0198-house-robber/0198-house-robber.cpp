0f2824dcaff8ed77b1e224e06fff059cbf119a72
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-10-20 16:52:44 +0530

    Time: 0 ms (100.00%), Space: 8 MB (50.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        //\U0001f972chori krne ka algo----
-        int n= nums.size();
-        vector<int> dp(n, -1);
-        dp[0]= nums[0];
+        //tabulation  \U0001f927<robbing using algo>----
+//         int n= nums.size();
+//         vector<int> dp(n, -1);
+//         dp[0]= nums[0];
         
+//         for(int i=1; i<n; i++){
+
+//             int pick= nums[i];
+//             if(i-2>=0) pick+= dp[i-2];
+//             int notPick= dp[i-1];
+            
+//             dp[i]= max(pick, notPick);
+//         }
+        
+//         return dp[n-1];
+        //space optimized----
+        int n= nums.size();
+        int prev2= 0, prev1= nums[0];
         for(int i=1; i<n; i++){
 
-            int pick= nums[i];
-            if(i-2>=0) pick+= dp[i-2];
-            int notPick= dp[i-1];
-            
-            dp[i]= max(pick, notPick);
+            int cur= nums[i];
+            if(i>1) cur+= prev2;
+            cur= max(cur, prev1);
+            prev2= prev1;
+            prev1= cur;
         }
         
-        return dp[n-1];
+        return prev1;
     }
 };
