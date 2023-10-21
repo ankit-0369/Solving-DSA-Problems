c04069d7cf886b253dd3b9fa6d8a1fa4b03281e0
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-10-21 11:11:10 +0530

    Added solution - LeetHub

--- Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
@@ -7,15 +7,26 @@ using namespace std;
 class Solution
 {
 public:
+    // long long f(int n){
+    //     long long sum= n;
+    //     for(int i=1; i<=n/2; i++){
+    //         if(n%i==0){
+    //             sum+= i;
+    //         }
+    //     }
+    //     return sum;
+    // }
     long long sumOfDivisors(int n)
     {
-        // Write Your Code here
-        
+        // Write Your Code here---
         long long sum=0;
         for(int i=1; i<=n; i++){
-            sum+= (n/i)* i;
+            // sum+= f(i);
+            // cout<<i<<" "<<f(i)<<"\n";
+            //As In total sum i will occur floor(n/i) times.
+            sum+= i* floor(n/i);
+            
         }
-        
         return sum;
     }
 };
