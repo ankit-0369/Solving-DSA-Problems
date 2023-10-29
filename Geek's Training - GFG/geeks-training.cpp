674abd982c2de0e2e6594f340ef7b707a3a60719
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-10-29 19:27:13 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -33,11 +33,48 @@ class Solution {
     }
     int maximumPoints(vector<vector<int>>& points, int n) {
         // Code here----
-         vector<vector<int>> dp(n, vector<int> (4,-1));
-        return solve(n-1,3, points, dp);
+        //  vector<vector<int>> dp(n, vector<int> (4,-1));
+        // return solve(n-1,3, points, dp);
         
-        return 0;
+        //memorization-----
+        
+            vector<vector<int>> memo(n, vector<int> (4,-1));
+            memo[0][0]= max(points[0][1], points[0][2]);
+             memo[0][1]= max(points[0][0], points[0][2]);
+             memo[0][2]= max(points[0][0], points[0][1]);
+             memo[0][3]= max(points[0][0], max(points[0][1], points[0][2]));
+             for(int day=1; day<n; day++){
+                 
+                 for(int last= 0; last<4; last++){
+                     
+                     memo[day][last]=0;
+                     for(int task=0; task<3; task++){
+                        
+                         if(last!= task){
+                         memo[day][last]= max(memo[day][last], memo[day-1][task]+ 
+                         points[day][task]);
+                         }
+                     
+                     }
+                     
+                 }
+                 
+             }
+           
+             return memo[n-1][3];
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 };
 
 //{ Driver Code Starts.
