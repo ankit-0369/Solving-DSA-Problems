17afd0fe142f8ca16d65e278fa2d8557c9b63036
Ankit kumar <98079557+ankit-0369@users.noreply.github.com>
2023-12-02 18:06:58 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -6,62 +6,28 @@ using namespace std;
 class Solution {
   public:
     
- 
-    int solve(int day, int last, vector<vector<int>>& points, vector<vector<int>> &dp){
-        
-        if(day==0){
-            int maxi= 0;
-            for(int i=0; i<3; i++){
-                if(i!=last)
-                maxi= max(maxi, points[0][i]);
-               
-            }
-            return dp[0][last]= maxi;
-        }
-        
-        if(dp[day][last]!= -1) return dp[day][last];
-        
-        int maxi=0;
-        for(int i=0; i<3; i++){
-            if(i!= last){
-            int score= points[day][i];
-            int rec= (dp[day-1][i]==-1)? solve(day-1, i, points, dp) : dp[day-1][i];
-            maxi= max(score+rec, maxi);
-            }
-        }
-        return dp[day][last]=maxi;
-    }
     int maximumPoints(vector<vector<int>>& points, int n) {
-        // Code here----
-        //  vector<vector<int>> dp(n, vector<int> (4,-1));
-        // return solve(n-1,3, points, dp);
-        
-        //memorization-----
-        
-            vector<vector<int>> memo(n, vector<int> (4,-1));
-            memo[0][0]= max(points[0][1], points[0][2]);
-             memo[0][1]= max(points[0][0], points[0][2]);
-             memo[0][2]= max(points[0][0], points[0][1]);
-             memo[0][3]= max(points[0][0], max(points[0][1], points[0][2]));
-             for(int day=1; day<n; day++){
-                 
-                 for(int last= 0; last<4; last++){
-                     
-                     memo[day][last]=0;
-                     for(int task=0; task<3; task++){
-                        
-                         if(last!= task){
-                         memo[day][last]= max(memo[day][last], memo[day-1][task]+ 
-                         points[day][task]);
-                         }
-                     
-                     }
-                     
-                 }
-                 
-             }
-           
-             return memo[n-1][3];
+      vector<int> prev(4,0);
+      prev[0]= max(points[0][1], points[0][2]);
+      prev[1]= max(points[0][0], points[0][2]);
+      prev[2]= max(points[0][0], points[0][1]);
+      prev[3]= max(points[0][0], max(points[0][1], points[0][2]));
+      
+      for(int day=1; day<n; day++){
+          vector<int> cur(4,0);
+          for(int last=0; last<4; last++){
+              cur[last]=0;
+              for(int task=0; task<3; task++){
+                  if(task!= last){
+                      cur[last]= max(cur[last], points[day][task]+prev[task]);
+                  }
+              }
+          }
+          prev= cur;
+      }
+      
+      return prev[3];
+      
     }
     
     
